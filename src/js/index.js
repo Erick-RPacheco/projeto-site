dd92105df18b2af19c91bc8bfbe466b6f53908df
Erick Rodrigues Pacheco <44420998+Erick-RPacheco@users.noreply.github.com>
2023-09-30 21:56:04 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desselecionarbotao();
+        desselecionarpersonagem();
+        
+        botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado");
+    });
+});
+
+function desselecionarpersonagem() {
+    const personagemselecionado = document.querySelector(".personagem.selecionado");
+    personagemselecionado.classList.remove("selecionado");
+}
+
+function desselecionarbotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
