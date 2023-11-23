8528a90bccd990b67b20a021d2804824d394db49
Erick Rodrigues Pacheco <44420998+Erick-RPacheco@users.noreply.github.com>
2023-11-22 23:23:02 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,41 @@
-const botoes = document.querySelectorAll(".botao");
-const personagens = document.querySelectorAll(".personagem");
+const chaveDaApi = "34789e20d90744a380613801232311";
+const botaoDeBusca = document.querySelector(".btn-busca");
 
-botoes.forEach((botao, indice) => {
-    botao.addEventListener("click", () => {
-        desselecionarbotao();
-        desselecionarpersonagem();
-        
-        botao.classList.add("selecionado");
-        personagens[indice].classList.add("selecionado");
-    });
+botaoDeBusca.addEventListener("click", async () => {
+    const cidade = document.getElementById("input-busca").value
+
+    const dados = await buscarDadosDaCidade(cidade);
+
+    preencherDadosNaTela(dados, cidade)
 });
 
-function desselecionarpersonagem() {
-    const personagemselecionado = document.querySelector(".personagem.selecionado");
-    personagemselecionado.classList.remove("selecionado");
+async function buscarDadosDaCidade(cidade) {
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
+
+    const resposta = await fetch(apiUrl)
+    
+
+    const dados = resposta.json();
+
+    return dados;
 }
 
-function desselecionarbotao() {
-    const botaoSelecionado = document.querySelector(".botao.selecionado");
-    botaoSelecionado.classList.remove("selecionado");
-}
+function preencherDadosNaTela(dados, cidade) {
+    const temperatura = dados.current.temp_c;
+    const condicao = dados.current.condition.text;
+    const humidade = dados.current.humidity;
+    const velocidadeDoVento = dados.current.wind_kph;
+    const iconeCondicao = dados.current.condition.icon
 
+    document.getElementById("cidade").textContent = cidade;
+
+    document.getElementById("temperatura").textContent = `${temperatura} ºC`;
+
+    document.getElementById("condicao").textContent = condicao;
+
+    document.getElementById("humidade").textContent = `${humidade}%`;
+
+    document.getElementById("velocidades").textContent = `${velocidadeDoVento} km/h`;
+
+    document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
+}
